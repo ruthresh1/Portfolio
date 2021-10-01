04aa06e737938c1e1de310a737c4d085a60038fc
ruthreshkumar <ruthreshss@gmail.com>
2021-10-01 18:37:37 +0530

    house-1: js changes for dynamic html display

---
+++ js/housewarming.js
@@ -0,0 +1,19 @@
+function getQueryVariable(variable) {
+      var query = window.location.search.substring(1);
+      var vars = query.split("&");
+      for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) {
+	        return pair[1];
+        }
+    }
+    alert('Query Variable ' + variable + ' not found');
+}
+
+document.write("<br />Name = " + getQueryVariable("name"));
+
+var name = getQueryVariable("name");
+
+document.getElementById("name").innerHTML = name;
+
+alert(name);
