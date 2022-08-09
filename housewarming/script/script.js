6408bc74b1dbd5a30c2071f6a1c16241fe211bb3
ruthreshkumar <ruthreshss@gmail.com>
2022-08-09 20:34:18 +0530

    aug2022: first deploy draft

---
+++ housewarming/script/script.js
@@ -0,0 +1,40 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+console.log(header)
+window.onscroll = function() {
+    const top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+// collapse navbar after click on small devices
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+})
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+      var pair = vars[i].split("=");
+      if (pair[0] == variable) {
+          return pair[1];
+      }
+  }
+  return "You"
+}
+
+var visitorName = getQueryVariable("name");
+visitorName = correctName(visitorName);
+document.getElementById("name").innerHTML = visitorName;
+
+function correctName(name) {
+name = name.replace("%20", " ").replace(/[^a-zA-Z .]+/g,'');
+return name;
+}
